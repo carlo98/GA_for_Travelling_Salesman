701030c58746125b2198eae4d520ca74c33ab9f6
Carlo Cena <carlo.cena@protonmail.com>
2019-05-24 15:58:48 +0200

    First commit

--- TestApp/testApp.py
+++ TestApp/testApp.py
@@ -1,4 +1,11 @@
 from TSP.TSP import TSP
 if __name__ == '__main__':
-    map = {"prima": {"seconda": 2, "quarta": 10}, "seconda": {"terza": 1, "prima": 2}, "terza": {}, "quarta": {"prima": 5}}
+    map = {"prima": {"seconda": 2, 
+                     "quarta": 10}, 
+           "seconda": {"terza": 1, 
+                       "prima": 2}, 
+           "terza": {"quinta": 5}, 
+           "quarta": {"prima": 5},
+           "quinta": {"seconda": 4, 
+                      "terza": 1}}
     TSP(map)
